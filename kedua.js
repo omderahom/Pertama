7738b60647a5b0ed69496a895b42d6a7fca9ed07
omderahom <kratonrahman2@gmail.com>
2025-01-30 10:31:35 +0700

    Add files via upload

---
+++ kedua.js
@@ -0,0 +1,37 @@
+<!-- No. berapa Start  Schema Markup  JSON-LD  -->
+<script type='application/ld+json'>
+{
+  &quot;@context&quot;: &quot;https://schema.org&quot;,
+  &quot;@type&quot;: &quot;Blog&quot;,
+  &quot;mainEntityOfPage&quot;: {
+    &quot;@type&quot;: &quot;WebPage&quot;,
+    &quot;@id&quot;: &quot;https://collectuber.blogspot.com/&quot;
+  },
+  &quot;headline&quot;: &quot;collectuber&quot;,
+  &quot;description&quot;: &quot;Best and Latest Video Collection for You.&quot;,
+  &quot;author&quot;: {
+    &quot;@type&quot;: &quot;Person&quot;,
+    &quot;name&quot;: &quot;Kusrahman&quot;
+  },
+  &quot;publisher&quot;: {
+    &quot;@type&quot;: &quot;Organization&quot;,
+    &quot;name&quot;: &quot;collectuber&quot;,
+    &quot;logo&quot;: {
+      &quot;@type&quot;: &quot;ImageObject&quot;,
+      &quot;url&quot;: &quot;https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgJAN_8HhgC3HeCLTLS28y1OTWdTrl_RB06liR-SN4HtaLw8ZNR1SYsaHINNjkk_2KjdIJfHnBvs7Sjy3LE26g3vSnn9reG2GimxZWjS_4hojz7RGv2pz42DX2Qdz4Q2B8XnLIN3BGruYPNlf8wpCVL5-dPBPkgjdg3h9peQf09bo28NEmtefuVZ3Uzrw/s1600/favicon-16x16.png&quot;
+    }
+  },
+  &quot;datePublished&quot;: &quot;2024-01-01&quot;,
+  &quot;dateModified&quot;: &quot;2024-01-01&quot;
+}
+</script>
+<!-- No. berapa End Schema Markup  JSON-LD  -->
+<!-- No. berapa Start Google tag (gtag.js) -->
+<script async='async' src='https://www.googletagmanager.com/gtag/js?id=GA-TRACKING-ID'/>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag(&#39;js&#39;, new Date());
+  gtag(&#39;config&#39;, &#39;GA-TRACKING-ID&#39;);
+</script>
+<!-- No. berapa End Google tag (gtag.js) -->
